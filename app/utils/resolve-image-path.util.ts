02412f9333f9cb80f475765a83a87f44923b000e
aenewsss <aenamartinelli@gmail.com>
2024-03-15 20:30:29 -0300

    testando novo path

--- app/utils/resolve-image-path.util.ts
+++ app/utils/resolve-image-path.util.ts
@@ -2,6 +2,6 @@ import { PublicPathsEnum } from "../enums/public-paths.enum";
 import { TypeImageEnum } from "../enums/type-image.enum";
 
 export function resolveImagePath(filename: string, type: TypeImageEnum) {
-    if (type == TypeImageEnum.EVENT) return PublicPathsEnum.EVENTS.replace('public', "/") + "/" + filename
-    return PublicPathsEnum.GALLERY.replace('public', "/") + "/" + filename
+    if (type == TypeImageEnum.EVENT) return "/" + PublicPathsEnum.EVENTS.replace('public', "/") +  filename
+    return "/" + PublicPathsEnum.GALLERY.replace('public', "/") +  filename
 }
