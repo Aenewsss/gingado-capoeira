812f9c265df8c32cf70aebc828da858b11ba33ac
Aenewsss <62079043+Aenewsss@users.noreply.github.com>
2024-08-28 18:07:45 -0300

    change gap

--- app/sections/Events.tsx
+++ app/sections/Events.tsx
@@ -23,9 +23,9 @@ export default function Events() {
         <section className="pt-20 bg-white flex justify-center flex-col" id="eventos">
             <h2 className="text-4xl lg:text-6xl text-center text-blue-950">Próximos Eventos</h2>
 
-            <div className="flex my-10 justify-center gap-14 sm:flex-row flex-col sm:px-0 px-4">
+            <div className="flex flex-wrap my-10 justify-center gap-14 sm:flex-row flex-col sm:px-0 px-4">
                 {events.map((event, index) =>
-                    <Image key={index} className="w-auto" width={500} height={500} src={resolveImagePath(event, TypeImageEnum.EVENT)} unoptimized alt="Evento 1" />
+                    <Image key={index} className="object-fit-contain" width={500} height={500} src={resolveImagePath(event, TypeImageEnum.EVENT)} unoptimized alt="Evento 1" />
                 )}
             </div>
 
