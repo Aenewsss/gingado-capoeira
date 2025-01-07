2a1eb17d0d1a6a711fd751c3138a3fc2eb7c85ba
Aenewsss <aenamartinelli@gmail.com>
2025-01-07 11:41:22 -0300

    fix: new transparencia page

--- app/transparencia/page.tsx
+++ app/transparencia/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Footer from "../components/Footer";
 import Example from "../components/Navbar";
+import Link from "next/link";
 
 export default function Page() {
     return (
@@ -8,26 +9,16 @@ export default function Page() {
             <Example />
 
 
-            <h1 className="text-center text-5xl my-10">Transparência | Recursos recebidos</h1>
+            <h1 className="text-center md:text-5xl text-3xl my-10 px-4">Transparência | Recursos recebidos</h1>
 
         
-            <section className="flex flex-col h-screen items-center justify-center mb-10">
+            <section className="flex flex-col items-center justify-center mb-10">
 
-                <Image className="mb-6" src="/logo-transparencia.jpeg" width={400} height={400} alt="Grupo logo" />
+                <Image quality={100} className="mb-6 md:w-auto w-full" src="/logo-transparencia.jpeg" width={400} height={400} alt="Grupo logo" />
+                <Image quality={100} className="mb-6 w-full md:w-3/4" src="/192.jpeg" width={400} height={300} alt="Grupo logo" />
+                <Image quality={100} className="mb-6 w-full md:w-3/4" src="/300.jpeg" width={400} height={300} alt="Grupo logo" />
 
-                <object
-                    data="/Recursos Recebidos.pdf#&navpanes=0&scrollbar=0"
-                    type="application/pdf"
-                    width="60%"
-                    height="100%"
-                    
-                >
-                    <p>
-                        Seu navegador não suporta a visualização de pdfs.
-                        <a href="Recursos Recebidos.pdf">Baixar pdf</a>
-                        .
-                    </p>
-                </object>
+                <Link className="text-xl mt-4 px-4 text-center text-blue-500" href="/gingado-capoeira.xlsx" download>Clicar e fazer o download da planilha</Link>
 
             </section>
 
